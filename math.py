3fdcbd698e509e9b9dbf8c041685e1c9e617e4cd
viddy09 <70717147+viddy09@users.noreply.github.com>
2021-08-02 14:31:41 +0530

    Math.py file is added

---
+++ math.py
@@ -0,0 +1,16 @@
+# Adding implementation
+def add(x,y):
+    return x+y
+
+# Substraction implementation
+def subtract(x,y):
+    return x-y      # impemented on master branch
+    
+# Multiplication implementation
+def multiply(x,y):
+    return x*y      # impemented on Bug456 branch
+    
+# Division implementation
+def divide(x,y):
+    return x/y      # implemented on master branch
+
